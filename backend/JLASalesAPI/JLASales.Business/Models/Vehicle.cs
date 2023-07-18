1f036371b4ebcf891280f7696d91bd2e3222aaca
Caio Porto <caioportoramos@gmail.com>
2023-07-18 15:15:08 -0300

    feat: added the Business and Data layers on backend

---
+++ backend/JLASalesAPI/JLASales.Business/Models/Vehicle.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLASales.Business.Models
+{
+    public class Vehicle : Entity
+    {
+        public string Name { get; set; }
+        public string EnginePower { get; set; }
+        public string ReleaseYear { get; set; }
+        public string Document { get; set; }
+
+
+    }
+}
---
+++ backend/JLASalesAPI/JLASales.Business/Models/Vendor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JLASales.Business.Models
+{
+    public class Vendor : Entity
+    {
+        public string Name { get; set; }
+
+        public string Document { get; set; }
+
+
+        public Address Address { get; set; }
+
+        public bool ActiveFlag { get; set; }
+
+        /* EF Relations: */
+
+        public IEnumerable<Sale> Sales { get; set; }
+    }
+}

ce0654a2083d15cf63f48618806a1ef3cbe200a0
Caio Porto <caioportoramos@gmail.com>
2023-07-21 13:32:25 -0300

    feat(backend): implemented data annotations in the Data Transfer Objects classes on the API layer

---
+++ backend/JLASalesAPI/src/JLASales.API/DTOs/SaleDTO.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JLASales.API.DTOs
+{
+    public class SaleDTO
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        public Guid VendorId { get; set; }
+
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+
+        public Guid VehicleId { get; set; }
+
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+
+        public Guid SaleValue { get; set; }
+
+
+        public string VendorName { get; set; }
+
+    }
+}
